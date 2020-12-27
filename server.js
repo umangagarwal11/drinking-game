60846ef8c92ec43c1ef41c9bc5c172f9a7f812e1
Umang <>
2020-12-27 05:43:02 +0530

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const {v4 : uuidv4} = require('uuid');
+
+const app = express();
+
+const server = require("http").Server(app);
+
+const io = require('socket.io')(server);
+
+const {ExpressPeerServer} = require ('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug:true
+});
+
+app.use(express.static('public'));
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.use("/peerjs", peerServer)
+
+app.get("/", (req,res) => {
+  res.render("index");
+});
+
+app.get("/play", (req,res) => {
+  res.redirect(`/play/${uuidv4()}`);
+});
+
+app.get("/play/:room", (req,res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+  });
+});
+
+server.listen(5000);
